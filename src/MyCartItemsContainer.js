d13068c849c3f5b9153f476ef58f56ef08f99614
Julien Hnery <jwh_917@yahoo.com>
2022-08-07 17:00:41 -0400

    Updated MyCartPage.js and MyCartItemsContainer.js

--- src/MyCartItemsContainer.js
+++ src/MyCartItemsContainer.js
@@ -3,7 +3,7 @@ import { ThemeContext } from "./theme";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function MyCartItemsContainer({total}) {
+function MyCartItemsContainer({total, myCartItems}) {
   
   const { theme } = useContext(ThemeContext);
 
@@ -14,6 +14,7 @@ function MyCartItemsContainer({total}) {
     <main >
       <div className="col-25" style={{textAlign: "center"}}>
         <div>
+          {myCartItems}
           <hr></hr>
           <p>Total <span className="price" style={{"color": textStyle}}><b>${total}</b></span></p>
         </div>
--- src/MyCartPage.js
+++ src/MyCartPage.js
@@ -18,7 +18,6 @@ function MyCart() {
   
     script.src = "https://kit.fontawesome.com/17a0ff6ff9.js";
     script.async = true;
-    // script.crossorigin = "anonymous";
   
     document.body.appendChild(script);
   
@@ -29,8 +28,6 @@ function MyCart() {
 
 
   const [myCart, setMyCart] = useState([])
-
-  // const [total, set] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3000/myCart")
@@ -45,11 +42,96 @@ function MyCart() {
   const sumTotal = myCart.map((myCartItem) => myCartItem.item.buy)
   const total = sumTotal.reduce((partialSum, a) => partialSum + a, 0);
 
-
-
   const { theme } = useContext(ThemeContext);
 
   const textStyle = theme ? "white" : "black"
+
+
+  function itemSizing(item){
+    if(item.item.category === "sneakers"){
+
+      return (
+        <select name="size" >
+          <option value="7">7</option>        
+          <option value="7.5">7.5</option>
+
+          <option value="8">8</option>
+          <option value="8.5">8.5</option>
+
+          <option value="9">9</option>
+          <option value="9.5">9.5</option>
+
+          <option value="10">10</option>
+          <option value="10.5">10.5</option>
+
+          <option value="11">11</option>
+          <option value="11.5">11.5</option>
+
+          <option value="12">12</option>
+          <option value="12.5">12.5</option>
+
+          <option value="13">13</option>
+          <option value="13.5">13.5</option>
+        </select>
+      )
+    }
+
+    if(item.item.category === "apparel"){
+      return(
+        <select name="size" >
+          <option value="Small">S</option>
+          <option value="Medium">M</option>
+          <option value="Large">L</option>
+          <option value="X-Large">XL</option>
+        </select>
+      )
+    }
+
+    if(item.item.category === "electronics"){
+      return(
+        <select name="size" >
+          <option value="NoSize">No Size</option>
+
+        </select>
+      )
+    }
+
+  }
+
+
+  const myCartItems = myCart.map((myCartItem) => {
+    console.log(myCartItem)
+    return(
+
+      <div key={myCartItem.id}>
+
+        <div>
+          <img src={myCartItem.item.itemUrl} alt="my cart item " style={{width:"50px", height:"50px"}}/>
+          <br></br>
+          {myCartItem.item.name}
+          <p>{myCartItem.item.color}</p>
+          
+          {itemSizing(myCartItem)}
+          <br></br>
+          
+          <span className="price">${myCartItem.item.buy}</span>
+
+          <br></br>
+
+          <span>🗑</span>
+
+        </div>
+        <br></br>
+      </div>
+      
+    )
+
+  })
+
+
+
+
+  
 
 
   return (
@@ -58,17 +140,7 @@ function MyCart() {
 
       <MyCartHeader itemsCount={itemsCount}/>
 
-
-      {/* myCartItemsContainer */}
-      <MyCartItemsContainer total={total}/>
-
-      {/* myCartItemsContainer */}
-      {/* <div className="col-25" style={{textAlign: "center"}}>
-        <div>
-          <hr></hr>
-          <p>Total <span className="price" style={{"color": textStyle}}><b>${total}</b></span></p>
-        </div>
-      </div> */}
+      <MyCartItemsContainer total={total} myCartItems={myCartItems}/>
   
       <br></br>
       <br></br>
