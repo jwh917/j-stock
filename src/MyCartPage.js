92e95262b1520b7cd47d0bb5479d56d1838607c2
Julien Hnery <jwh_917@yahoo.com>
2022-08-08 07:03:58 -0400

    Updated UserInputContainer.js and MyCartPage.js

--- src/MyCartPage.js
+++ src/MyCartPage.js
@@ -28,18 +28,26 @@ function MyCartPage() {
 
   const [myCart, setMyCart] = useState([])
 
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [address, setAddress] = useState("")
+  const [city, setCity] = useState("")
+  const [state, setState] = useState("")
+  const [zipCode, setZipCode] = useState("")
+
+
+  const [cardName, setCardName] = useState("")
+  const [cardNum, setCardNum] = useState("")
+  const [expMon, setExpMon] = useState("")
+  const [expYr, setExpYr] = useState("")
+  const [cvv, setCvv] = useState("")
+
+
   useEffect(() => {
     fetch("http://localhost:3000/myCart")
       .then((r) => r.json())
       .then((myCartData) => setMyCart(myCartData));
   }, []);
-
-  function handleDelItem(deletedItem){
-    console.log(deletedItem)
-    const updatedCart = myCart.filter((item) => item.id !== deletedItem.id);
-    setMyCart(updatedCart)
-  }
-
 
   const itemsCount = myCart.length
 
@@ -57,10 +65,95 @@ function MyCartPage() {
       myCartItem={myCartItem}
       handleDelItem={handleDelItem}
       />
-      
     )
-
   })
+
+  function handleDelItem(deletedItem){
+    const updatedCart = myCart.filter((item) => item.id !== deletedItem.id);
+    setMyCart(updatedCart)
+  }
+
+  function fullNameInput(event){
+    setFullName(event.target.value)
+  }
+
+  function emailInput(event){
+    setEmail(event.target.value)
+  }
+
+  function addressInput(event){
+    setAddress(event.target.value)
+  }
+
+    function cityInput(event){
+    setCity(event.target.value)
+  }
+
+    function stateInput(event){
+    setState(event.target.value)
+  }
+
+    function zipCodeInput(event){
+    setZipCode(event.target.value)
+  }
+
+    function cardNameInput(event){
+    setCardName(event.target.value)
+  }
+
+    function cardNumInput(event){
+    setCardNum(event.target.value)
+  }
+
+    function expMonInput(event){
+    setExpMon(event.target.value)
+  }
+
+      function expYrInput(event){
+    setExpYr(event.target.value)
+  }
+
+        function cvvInput(event){
+    setCvv(event.target.value)
+  }
+
+
+
+  function formSumbitHandle(event){
+    event.preventDefault()
+
+    const newCompletedOrder = {
+      fullName: fullName,
+      email: email,
+      address: address,
+      city: city,
+      state: state,
+      zipCode: zipCode,
+      cardName: cardName,
+      cardNum: cardNum,
+      expMon: expMon,
+      expYr: expYr,
+      cvv: cvv,
+      myCart: myCart,
+      total: total
+    }
+
+    if(newCompletedOrder.fullName === "" || newCompletedOrder.email === "" || newCompletedOrder.email === "" || newCompletedOrder.address === "" || newCompletedOrder.city === "" || newCompletedOrder.state === "" || newCompletedOrder.zipCode === "" || newCompletedOrder.cardName === "" || newCompletedOrder.cardNum === "" || newCompletedOrder.expMon === "" || newCompletedOrder.expYr === "" || newCompletedOrder.cvv === "" || newCompletedOrder.myCart.length === 0 || newCompletedOrder.total === 0) return
+
+    fetch("http://localhost:3000/completedOrders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCompletedOrder),
+    })
+      .then((r) => r.json())
+      .then((newCompletedOrderData) => console.log(newCompletedOrderData));
+
+      // clear form
+      // clear myCart Array
+
+  }
 
   return (
     <main style={{"color": textStyle}}>
@@ -72,7 +165,7 @@ function MyCartPage() {
       <br></br>
       <br></br>
 
-      <UserInputContainer/>
+      <UserInputContainer fullNameInput={fullNameInput} emailInput={emailInput} addressInput={addressInput} cityInput={cityInput} stateInput={stateInput} zipCodeInput={zipCodeInput} cardNameInput={cardNameInput} cardNumInput={cardNumInput} expMonInput={expMonInput} expYrInput={expYrInput} cvvInput={cvvInput} formSumbitHandle={formSumbitHandle}/>
       <CheckoutBar total={total}/>
     </main>
   );
--- src/UserInputContainer.js
+++ src/UserInputContainer.js
@@ -2,34 +2,35 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function UserInputContainer() {
+function UserInputContainer({fullNameInput, emailInput, addressInput, cityInput, stateInput, zipCodeInput, cardNameInput, cardNumInput, expMonInput, expYrInput, cvvInput, formSumbitHandle}) {
 
 
   return (
     <main >
       <div className="container">
-        <form action="/action_page.php" id="my-form">
+        <form action="/action_page.php" id="my-form" onSubmit={formSumbitHandle}>
+        {/* <form id="my-form" onSubmit={formSumbitHandle}> */}
 
           <div className="grid-child"> 
             <h3>Billing Address</h3>
             <label htmlFor="fname"><i className="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe"/>
+            <input type="text" id="fname" name="firstname" placeholder="John M. Doe" onChange={fullNameInput}/>
             <label htmlFor="email"><i className="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com"/>
+            <input type="text" id="email" name="email" placeholder="john@example.com" onChange={emailInput}/>
             <label htmlFor="adr"><i className="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
+            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" onChange={addressInput}/>
             <label htmlFor="city"><i className="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York"/>
+            <input type="text" id="city" name="city" placeholder="New York" onChange={cityInput}/>
 
             <div className="row">
               <div className="col-50">
                 <label htmlFor="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY"/>
+                <input type="text" id="state" name="state" placeholder="NY" onChange={stateInput}/>
               </div>
               <div className="col-50">
                 <label htmlFor="zip">Zip</label>
                 <br></br>
-                <input type="text" id="zip" name="zip" placeholder="10001"/>
+                <input type="text" id="zip" name="zip" placeholder="10001" onChange={zipCodeInput}/>
               </div>
             </div>
           </div>
@@ -47,20 +48,20 @@ function UserInputContainer() {
               <i className="fa fa-cc-discover" style={{color:"orange"}}></i>
             </div>
               <label htmlFor="cname">Name on Card</label>
-              <input type="text" id="cname" name="cardname" placeholder="John More Doe"/>
+              <input type="text" id="cname" name="cardname" placeholder="John More Doe" onChange={cardNameInput}/>
               <label htmlFor="ccnum">Credit card number</label>
-              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" onChange={cardNumInput}/>
               <label htmlFor="expmonth">Exp Month</label>
-              <input type="text" id="expmonth" name="expmonth" placeholder="September"/>
+              <input type="text" id="expmonth" name="expmonth" placeholder="September" onChange={expMonInput}/>
               <div className="row">
                 <div className="col-50">
                   <label htmlFor="expyear">Exp Year</label>
-                  <input type="text" id="expyear" name="expyear" placeholder="2018"/>
+                  <input type="text" id="expyear" name="expyear" placeholder="2018" onChange={expYrInput}/>
                 </div>
                 <div className="col-50">
                   <label htmlFor="cvv">CVV</label>
                   <br></br>
-                  <input type="text" id="cvv" name="cvv" placeholder="352"/>
+                  <input type="text" id="cvv" name="cvv" placeholder="352" onChange={cvvInput}/>
                 </div>
               </div>
           
